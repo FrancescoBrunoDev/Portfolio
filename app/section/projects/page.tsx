9213579b73af27db7072113aee9924ebf26a356e
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2024-04-24 11:49:41 +0200

    Aggiornato devProjects.json e otherProjects.json con nuovi progetti

--- app/section/projects/page.tsx
+++ app/section/projects/page.tsx
@@ -6,24 +6,30 @@ export default function Project() {
   return (
     <div className="h-screen w-screen items-center pt-12 text-primary">
       <div className="container grid h-full w-screen grid-rows-2 gap-10 py-8 ">
-        <div className="flex h-full flex-col content-stretch lg:flex-row lg:items-center overflow-hidden">
+        <div className="flex h-full flex-col content-stretch overflow-hidden lg:flex-row lg:items-center">
           <div className="flex h-1/3 items-center pr-10 text-8xl font-semibold uppercase lg:h-auto">
             Dev
           </div>
           <div className="flex h-2/3 w-full gap-2 overflow-x-auto lg:h-full">
-            {devProjects.map((project) => (
-              <ProjectItem key={project.id} item={project} />
-            ))}
+            {devProjects.map(
+              (project) =>
+                project.hidden === false && (
+                  <ProjectItem key={project.id} item={project} />
+                )
+            )}
           </div>
         </div>
-        <div className="flex h-full flex-col content-stretch lg:flex-row lg:items-center overflow-hidden">
+        <div className="flex h-full flex-col content-stretch overflow-hidden lg:flex-row lg:items-center">
           <div className="flex h-1/3 items-center pr-10 text-8xl font-semibold uppercase lg:h-auto">
             Other
           </div>
           <div className="flex h-2/3 w-full gap-2 overflow-x-auto lg:h-full">
-            {otherProjects.map((project) => (
-              <ProjectItem key={project.id} item={project} />
-            ))}
+            {otherProjects.map(
+              (project) =>
+                project.hidden === false && (
+                  <ProjectItem key={project.id} item={project} />
+                )
+            )}
           </div>
         </div>
       </div>
