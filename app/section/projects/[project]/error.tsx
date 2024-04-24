0d0fde85c3aa0349121c6341146d8344f45943eb
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2024-04-24 13:44:49 +0200

    Fix styling in not-found.tsx and [project]/error.tsx

--- app/section/projects/[project]/error.tsx
+++ app/section/projects/[project]/error.tsx
@@ -7,9 +7,9 @@ import { Button } from "@/components/ui/button";
 export default function Error() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <div className="flex w-44 flex-wrap justify-center gap-4 ">
+      <div className="flex flex-col items-center gap-4 ">
         <Brain className="h-24 w-24 stroke-primary stroke-[2.5]" />
-        <h2 className="text-center text-xl text-primary">
+        <h2 className="text-center w-44 text-xl text-primary">
           Whether it's a happy accident or a testament to your brilliant mind
           (♥), care to return to exploring my projects?
         </h2>
