c07f949f9540b44334e97021bf54b225d91c2972
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2024-05-13 13:15:02 +0200

    Refactor fetchBooksInfo.tsx to use lazy loading for book images
    The code changes in `page.tsx` introduce lazy loading for book images using the `Suspense` component from React. This improves the page load performance by deferring the loading of images until they are actually needed.
    
    This commit message follows the convention of starting with a verb in the imperative form (`Refactor`) to describe the purpose of the changes.

--- app/section/record/books/page.tsx
+++ app/section/record/books/page.tsx
@@ -1,7 +1,29 @@
 import BooksView from "@/components/books/booksView";
 import fetchBooksInfo from "@/components/books/fetchBooksInfo";
+import { Suspense } from 'react';
+import { BookCopy } from "lucide-react";
 
-export default async function Page() {
+async function View() {
   const data = await fetchBooksInfo();
   return <BooksView booksFetch={data} />;
+}
+
+function LoadingBooks() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <BookCopy className="h-24 w-24 stroke-primary stroke-[2.5] animate-pulse" />
+        <h2 className="text-center w-44 text-xl text-primary">
+          Just a tick, I'll grab the books from the shelves.
+        </h2>
+      </div>
+    </div>
+  )
+}
+
+export default async function Page() {
+  return (
+    <Suspense fallback={<LoadingBooks />}>
+      <View />;
+    </Suspense>)
 }
