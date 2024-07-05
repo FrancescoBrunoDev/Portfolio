004170d283d465d8ff3c22f8c0136d5e8eaff0f6
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2024-07-05 10:09:32 +0200

    added redirect for certifications

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   images: {
     remotePatterns: [
       {
@@ -9,6 +10,20 @@ const nextConfig = {
         pathname: "/books/**",
       },
     ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/ffc/:path",
+        destination: "https://www.freecodecamp.org/certification/fcc1f21e1cd-ef52-48f7-8fe5-b48b50dd9066/:path",
+        permanent: false
+      },
+      {
+        source: "/udemy/:path",
+        destination: "https://www.udemy.com/certificate/:path",
+        permanent: false
+      }
+    ];
   },
 };
 
