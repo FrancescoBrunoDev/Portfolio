ad24fb74552da46e9ba4f7a80668c7082d45bca4
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2024-07-30 14:14:35 +0200

    chore: Improve BookItem component readability and fix typos

--- components/books/item.tsx
+++ components/books/item.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import ModalInfoBook from "@/components/books/modalInfoBook";
-import Image from "next/image"
+import Image from "next/image";
 
 export default function BookItem(book: Book) {
   const [isopen, setIsOpen] = useState(false);
@@ -11,6 +11,8 @@ export default function BookItem(book: Book) {
   const altNotes = book.item.altNotes;
   const tranlatedNotes = book.item.tranlatedNotes;
   const titleParts = infoBooks.title.match(/[^.!]+[.!]?/g) || [];
+
+  const lenghtMainTitle = 50;
 
   return (
     <motion.div
@@ -22,7 +24,7 @@ export default function BookItem(book: Book) {
       <button
         onClick={() => setIsOpen(true)}
         key={infoBooks.industryIdentifiers[0].identifier}
-        className="relative z-0 h-56 w-32 shrink-0 translate-x-0 border-2 border-primary transition-all duration-200 ease-in-out hover:scale-[1.025] overflow-visible"
+        className="relative z-0 h-56 w-32 shrink-0 translate-x-0 overflow-visible border-2 border-primary transition-all duration-200 ease-in-out hover:scale-[1.025]"
       >
         <div className="h-full w-full bg-primary p-3 opacity-100 transition-opacity duration-300">
           <div className="flex h-full flex-col text-left text-background">
@@ -32,16 +34,25 @@ export default function BookItem(book: Book) {
                 {index < infoBooks.authors.length - 1 && <br />}
               </span>
             ))}
-            {note.length !== 0 && Object.keys(note).length > 1 &&
+            {note.length !== 0 && Object.keys(note).length > 1 && (
               <div className="pt-6">
-                <Image width={300} height={300} alt="sketch holdplace" src="/books/sketches/1.svg" />
+                <Image
+                  width={300}
+                  height={300}
+                  alt="sketch holdplace"
+                  src="/books/sketches/1.svg"
+                />
               </div>
-            }
+            )}
           </div>
         </div>
         <div className="absolute bottom-0 left-0 right-0 z-10 border-t-2 border-primary bg-background p-2 text-left text-base font-semibold uppercase">
-          <div className="text-sm">{titleParts[0]}</div>
-          <div className="text-xs lowercase truncate">{titleParts[1]}</div>
+          <div className="text-sm">
+            {titleParts[0] && titleParts[0].length > lenghtMainTitle
+              ? `${titleParts[0].substring(0, lenghtMainTitle)}...`
+              : titleParts[0]}
+          </div>
+          <div className="truncate text-xs lowercase">{titleParts[1]}</div>
         </div>
       </button>
       {/* Modal */}
