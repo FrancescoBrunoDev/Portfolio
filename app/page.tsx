4a656786d2cd1a4ddd0a2472aaeb3d5cf6a3372c
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2024-08-11 19:18:23 +0200

    chore: Add self-hosted component to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Landing from "@/components/landing";
+import SelfHosted from "@/components/seltHosted";
 
 export default function Home() {
   return (
     <main>
       <Landing />
+      {process.env.IS_SELF_HOSTED === "true" && <SelfHosted />}
     </main>
   );
 }
---
+++ components/seltHosted.tsx
@@ -0,0 +1,7 @@
+export default function SelfHosted() {
+  return (
+    <div className="fixed bottom-0 z-50 h-8 w-full text-center text-sm text-primary/50">
+      proudly self-hosted
+    </div>
+  );
+}
