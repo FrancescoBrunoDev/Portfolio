10c4de856513b02e79110483d9b7ed7c77d65016
Francesco Bruno <francesco.bruno001@gmail.com>
2024-09-07 10:49:48 +0000

    chore: Add pointer-events-none to self-hosted component

--- components/seltHosted.tsx
+++ components/seltHosted.tsx
@@ -1,6 +1,6 @@
 export default function SelfHosted() {
   return (
-    <div className="fixed bottom-0 z-50 h-8 w-full text-center text-sm text-primary/50">
+    <div className="fixed bottom-0 z-50 h-8 w-full text-center text-sm text-primary/50 pointer-events-none">
       proudly self-hosted
     </div>
   );
