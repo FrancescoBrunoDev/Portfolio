958acb979f30d4a97815a6d0a5859ad2d620ea64
Francesco Bruno <francesco.bruno001@gmail.com>
2024-11-28 08:00:32 +0000

    navbar improved

--- components/nav.tsx
+++ components/nav.tsx
@@ -10,26 +10,51 @@ export default function Nav() {
   if (pathname === "/") {
     return null;
   }
+  const sections = [
+    {
+      name: "About",
+      href: "/section/about/",
+    },
+    {
+      name: "Projects",
+      href: "/section/projects/",
+    },
+    {
+      name: "Record",
+      href: "/section/record/",
+    },
+  ];
   return (
     <AnimatePresence>
-      <header className="container fixed z-20 h-14 w-screen bg-background py-3 text-primary">
-        <motion.div
-          key="navbar"
-          initial={{ y: -5, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          exit={{ y: 10, opacity: 0 }}
-        >
-          <div className="flex items-center justify-between">
-            <Link href={"/"}>
-              <Logo />
-            </Link>
-            <div className="flex justify-around gap-4">
-              <Link href={"/section/about/"}>About</Link>/
-              <Link href={"/section/projects/"}>Projects</Link>/
-              <Link href={"/section/record/"}>Record</Link>
+      <header className="fixed z-20 h-14 w-screen bg-background py-3 text-primary">
+        <div className="container">
+          <motion.div
+            key="navbar"
+            initial={{ y: -5, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: 10, opacity: 0 }}
+          >
+            <div className="flex items-center justify-between">
+              <Link href={"/"}>
+                <Logo />
+              </Link>
+              <div className="flex justify-around gap-4">
+                {sections.map((section, index) => (
+                  <>
+                    <Link
+                      key={section.href}
+                      className="transition-all duration-100 ease-in-out hover:font-semibold"
+                      href={section.href}
+                    >
+                      {section.name}
+                    </Link>
+                    {index < sections.length - 1 && <span className="">/</span>}
+                  </>
+                ))}
+              </div>
             </div>
-          </div>
-        </motion.div>
+          </motion.div>
+        </div>
       </header>
     </AnimatePresence>
   );
