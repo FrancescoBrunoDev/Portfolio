b49b01b4b74f0d4e950e5d00b230d6c786c57df5
Francesco Bruno <francesco.bruno001@gmail.com>
2024-11-28 08:14:48 +0000

    better img handling in projects

--- components/projects/item.tsx
+++ components/projects/item.tsx
@@ -8,7 +8,7 @@ export default function ProjectItem({ project }: { project: Project }) {
       href={`/section/projects/${project.$id}`}
       className="relative z-0 h-full w-32 shrink-0 translate-x-0 border-2 border-primary transition-all duration-300 ease-in-out lg:border-none lg:text-transparent lg:hover:z-10 lg:hover:mr-6 lg:hover:translate-x-6 lg:hover:text-primary"
     >
-      <div className="absolute h-full w-full bg-primary p-3 opacity-100 transition-opacity duration-300 hover:opacity-0">
+      <div className="absolute z-10 h-full w-full bg-primary p-3 opacity-100 transition-opacity duration-300 hover:opacity-30">
         <div className="flex h-full items-start lg:items-end">
           <div className="hidden rotate-180 flex-col items-center gap-1 lg:flex">
             <Fingerprint className="mr-0.5 h-3 w-3 rotate-90 stroke-background stroke-[3]" />
@@ -22,9 +22,9 @@ export default function ProjectItem({ project }: { project: Project }) {
       <Image
         src={project.image}
         alt={project.title}
-        height={500}
-        width={500}
-        className="z-0 h-full object-cover"
+        fill={true}
+        sizes="40vw"
+        className="z-0 object-cover"
       />
       <div className="absolute bottom-0 z-10 w-full border-t-2 border-primary bg-background p-2 text-base font-semibold uppercase lg:w-96 lg:origin-bottom-left lg:rotate-[270deg] lg:truncate lg:rounded-none lg:border-none lg:bg-transparent lg:p-0 lg:text-lg">
         {project.title}
