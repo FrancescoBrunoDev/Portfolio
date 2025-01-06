029fafcccdbd144edafce736bead122ed3463fec
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2025-01-06 18:52:26 +0000

    hide stravaInfo

--- app/section/record/page.tsx
+++ app/section/record/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
-import StravaInfo from "@/components/strava/stravaInfo";
+// import StravaInfo from "@/components/strava/stravaInfo";
 
 export default function Page() {
   return (
     <div className="container relative flex h-screen items-center text-primary">
-      <div className="container absolute inset-x-0 top-20 z-0 flex justify-end">
+      {/* <div className="container absolute inset-x-0 top-20 z-0 flex justify-end">
         <StravaInfo className="h-fit" />
-      </div>
+      </div> */}
       <div className="w-full">
         <div className="flex flex-col justify-center pt-10 text-5xl font-normal uppercase sm:text-7xl md:text-8xl lg:justify-normal">
           <Link
