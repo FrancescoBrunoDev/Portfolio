6b28b96089e015671ee939de18b14a0e6becae03
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2025-06-17 22:03:15 +0200

    added umami for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { outfit } from "@/components/text";
 import Nav from "@/components/nav";
 import AnimatedCursorWrapper from "@/components/AnimatedCursorWrapper";
+import Script from "next/script";
 
 export const metadata = {
   title: "Francesco Bruno",
@@ -17,6 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          defer
+          src="http://umami-w4w0gwowc4kgg4gogs0o8sok.francesco-bruno.com/script.js"
+          data-website-id="689235d0-8491-4c50-9aaa-54ea7018115a"
+        />
+      </head>
       <body className={outfit.className}>
         <Nav />
         <AnimatedCursorWrapper />
