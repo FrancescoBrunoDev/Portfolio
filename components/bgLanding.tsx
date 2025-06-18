035bda32a319d3b95264b086423dc6c4e3b16f02
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2025-06-18 10:43:17 +0200

    Update transition delay for improved loading effect in BgLanding component

--- components/bgLanding.tsx
+++ components/bgLanding.tsx
@@ -38,7 +38,7 @@ export const BgLanding: React.FC<BgLandingProps> = ({
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ delay: 1, duration: 1 }}
+        transition={{ delay: 2, duration: 1 }}
         style={{ willChange: "transform" }}
       >
         <svg
